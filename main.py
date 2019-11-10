c30cb2d7fc2fef06afbd44f817b4c932ecbe70aa
Saurabh Narain <saurabh@Saurabhs-MacBook-Pro.local>
2019-11-10 01:35:11 -0800

    changed redis_tester to work with updated channels and see what output looks like

--- main.py
+++ main.py
@@ -2,10 +2,6 @@ import functools
 import os
 import sys
 import redis
-from configparser import ConfigParser
-from select import poll
-from select import POLLERR
-from select import POLLHUP
 from flask import Flask
 from flask import render_template
 
@@ -24,61 +20,23 @@ def subscribe(host, password, *channels):
     sub.subscribe(channels)
     return sub
 
-def parse_printer(status_message):
-    return status_message.split()[-1].strip('.')
-
 printer_dict = {'logjam': ['test_user1'], 'pagefault': ['test_user2'], 'papercut': ['test_user3']}
 
-def add_to_printer_dict (username, printer):
-    if printer == 'logjam':
-        printer_dict['logjam'].append(username)
-    elif printer == 'pagefault':
-        printer_dict['pagefault'].append(username)
-    elif printer == 'papercut':
-        printer_dict['papercut'].append(username)
-
-def main():
-    username = os.environ.get('USER')
-
-    # read config file
+def monitor_printer(printer):
     host, password = 'broker.ocf.berkeley.edu', '###'
 
-    # set up stdout monitoring
-    p = poll()
-    p.register(sys.stdout.fileno(), POLLERR | POLLHUP)
-
-    s = subscribe(host, password, username)
+    s = subscribe(host, password, 'printer-' + printer)
     while True:
         message = s.get_message()
         if message and 'data' in message:
-            status_message = message['data'].decode(encoding='UTF-8').replace('\n', ' ')
-            test_file = open('testfile.txt', 'w')
-            print(username + ' | ' + parse_printer(status_message) + '\n', file = test_file, flush=True)
-            test_file.close()
-            add_to_printer_dict(username, parse_printer(status_message))
-        if p.poll(0.1):
-            break
+            user_list = message['data'].decode(encoding='UTF-8').replace('\n', ' ')
+            print(user_list) #need to see what the output is like
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
 
-"""
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return printer_dict
-    #return ', '.join(printer_dict['logjam'])
-
-@app.route('/logjam')
-def logjam():
-    return render_template('logjam.html')
-
-@app.route('/papercut')
-def papercut():
-    return render_template('papercut.html')
-
-@app.route('/pagefault')
-def pagefault():
-    return render_template('pagefault.html')
-"""
--- redis_tester.py
+++ redis_tester.py
@@ -2,16 +2,10 @@ import functools
 import os
 import sys
 import redis
-from configparser import ConfigParser
-from select import poll
-from select import POLLERR
-from select import POLLHUP
-from flask import Flask
-from flask import render_template
 
 redis_connection = functools.partial(
     redis.StrictRedis,
-    host='broker.ocf.berkeley.edu',
+    host='broker',
     port=6378,
     ssl=True,
     db=0,
@@ -24,21 +18,18 @@ def subscribe(host, password, *channels):
     sub.subscribe(channels)
     return sub
 
-def main():
-    # set up stdout monitoring
-    p = poll()
-    p.register(sys.stdout.fileno(), POLLERR | POLLHUP)
+def monitor_printer(printer):
+    host, password = 'broker.ocf.berkeley.edu', '###'
 
-    s = subscribe('job.user')
+    s = subscribe(host, password, 'printer-' + printer)
     while True:
         message = s.get_message()
         if message and 'data' in message:
-            status_message = message['data'].decode(encoding='UTF-8').replace('\n', ' ')
-            test_file = open('testfile.txt', 'w')
-            print(status_message, file = test_file, flush=True)
-            test_file.close()
-        if p.poll(0.1):
-            break
+            user_list = message['data'].decode(encoding='UTF-8').replace('\n', ' ')
+            print(user_list)
 
-if __name__ == "__main__":
+def main():
+    monitor_printer('pagefault')
+
+if __name__ == '__main__':
     main()
