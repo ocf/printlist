b81b04a2b2767073cddfbd5144a4376396eb4d6c
Saurabh Narain <snarain@ocf.berkeley.edu>
2019-11-15 15:20:31 -0800

    added timer functionality to remove users off the list after a set amount of time

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import functools
 import os
 import sys
+import time
 import redis
 from flask import Flask
 from flask import render_template
@@ -25,6 +26,14 @@ printer_dict = {'printer-logjam': [], 'printer-pagefault': [], 'printer-papercut
 def push_user(printer, username):
     printer_dict[printer].append(username)
 
+def remove_user(printer, username):
+    printer_dict[printer].remove(username)
+
+def check_user(printer, username):
+    curr_time = time.time()
+    if curr_time - username[1] >= 180:
+        remove_user(printer, username)
+
 def monitor_printer():
     host, password = 'broker.ocf.berkeley.edu', '###'
 
@@ -33,9 +42,12 @@ def monitor_printer():
         message = s.get_message()
         if message and 'data' in message:
             printer = message['channel'].decode(encoding='UTF-8').replace('\n', ' ')
-            username = message['data'].decode(encoding='UTF-8').replace('\n', ' ')
+            username = (message['data'].decode(encoding='UTF-8').replace('\n', ' '), time.time())
             push_user(printer, username)
             print(printer_dict) #temporary to see if things are working
+        for p in printer_dict.keys():
+            for u in printer_dict[p]:
+                check_user(p, u)
 
 if __name__ == '__main__':
     monitor_printer()
