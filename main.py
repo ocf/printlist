8b4d25b79265e351850e73c8e112ed190eb7313f
Wilson Nguyen <wilsonqnguyen@gmail.com>
2020-02-12 05:59:10 -0800

    Refactor for status support

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ import sys
 import time
 import redis
 import threading
+import json
 from configparser import ConfigParser
 from flask import Flask
 from flask import render_template
 from flask import jsonify
 from flask import request
-import sys
 
 # Contains Redis secrets
 BROKER_AUTH = 'conf/broker.conf'
@@ -27,11 +27,70 @@ redis_connection = functools.partial(
     password=None
 )
 
+printer_names = {
+    'printer-papercut': 'papercut',
+    'printer-logjam': 'logjam',
+    'printer-pagefault': 'pagefault'
+}
+print_jobs = {
+    'printer-papercut': {},
+    'printer-logjam': {},
+    'printer-pagefault': {}
+}
+
+persist = 180
+persist_completed = 60
+
+class Job:
+    @staticmethod
+    def cleanup():
+        for printer in print_jobs:
+            for job in printer:
+                if job.currentStatus() == 0:
+                    if job.time+persist_completed < time.time():
+                        del job
+                else:
+                    if job.time+persist < time.time():
+                        del job
+    @staticmethod
+    def getRecent(time):
+        jobs = {}
+        for printer in print_jobs:
+            jobs[printer] = {}
+            jobs[printer] = {job: print_jobs[printer][job] for job in print_jobs[printer] if job.last_updated > time}
+        return jobs
+
+    @staticmethod
+    def add(printer_name, username, time, status, job_id):
+        if job_id in print_jobs[printer_name]:
+            return print_jobs[printer_name][job_id].update(username, status, time)
+        print_jobs[printer_name][job_id] = Job(username, time, status, job_id)
+        Job.cleanup()
+
+    def __init__(self, username, time, status, job_id):
+        self.username = username
+        self.id = job_id
+        self.last_updated = time
+        self.statusQueue = [(status, time)]
+
+    def update(self, username, status, time):
+        if username != self.username:
+            print("Conflict: Job#" + job_id + " | " + self.username + " vs " + username);
+            return False
+        self.statusQueue.append((status, time))
+        self.last_updated = time
+        return True
+
+    def currentStatus(self):
+        return self.statusQueue[-1]
+
+
 def subscribe(host, password, *channels):
     rc = redis_connection(host=host, password=password)
     sub = rc.pubsub(ignore_subscribe_messages=True)
     sub.subscribe(channels)
     return sub
+
 
 def read_config():
     config = ConfigParser()
@@ -40,19 +99,6 @@ def read_config():
     password = config.get('broker', 'password')
     return host, password
 
-printer_dict = {'printer-logjam': [], 'printer-pagefault': [], 'printer-papercut': []}
-printer_names = [('printer-papercut', 'papercut'), ('printer-logjam', 'logjam'), ('printer-pagefault', 'pagefault')]
-
-def push_user(printer, username):
-    printer_dict[printer].append(username)
-
-def remove_user(printer, username):
-    printer_dict[printer].remove(username)
-
-def check_user(printer, username):
-    curr_time = time.time()
-    if curr_time - username[1] >= 180:
-        remove_user(printer, username)
 
 def monitor_printer():
     host, password = read_config()
@@ -61,13 +107,19 @@ def monitor_printer():
     while True:
         message = s.get_message()
         if message and 'data' in message:
-            printer = message['channel'].decode(encoding='UTF-8').replace('\n', ' ')
-            username = (message['data'].decode(encoding='UTF-8').replace('\n', ' '), time.time())
-            push_user(printer, username)
-            print(printer_dict) #temporary to see if things are working
-        for p in printer_dict.keys():
-            for u in printer_dict[p]:
-                check_user(p, u)
+            try:
+                printer_name = message['channel']
+                print_job = json.loads(message['data'])
+                Job.add(
+                    printer_name=printer_name,
+                    username=print_job.user,
+                    time=print_job.time,
+                    status=print_job.status,
+                    job_id=print_job.id
+                )
+            except ValueError:
+                print('Unable to parse JSON')
+
 
 def create_app():
     app = Flask(__name__)
@@ -100,10 +152,7 @@ def reload():
     if not request.args.get('last-fetch', '').isdigit():
         return 'Invalid Request', 400
     last_fetch = int(request.args.get('last-fetch'))/1000
-    recent = {}
-    for printer in printer_dict:
-        recent[printer] = [job for job in printer_dict[printer] if job[1] > last_fetch]
-    return jsonify(recent)
+    return jsonify(Job.getrecent(last_fetch))
 
 if DEV_MODE:
     app.config.update(TEMPLATES_AUTO_RELOAD = True, SEND_FILE_MAX_AGE_DEFAULT=0)
